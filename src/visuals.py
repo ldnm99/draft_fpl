b233ed930c03f1e6a288a7cfcad503621e685aa0
ldnm99 <lourencomarvao@gmail.com>
2024-11-16 23:29:11 +0000

    updated visuals

--- src/visuals.py
+++ src/visuals.py
@@ -14,7 +14,15 @@ def create_visuals():
     color='Team Name',
     title="Cumulative Points by Gameweek for Each Team",
     labels={'gameweek': 'Gameweek', 'Cumulative Points': 'Cumulative Points'},
-    hover_name='Team Name'
+    hover_name='Team Name',
+    line_shape='linear'
+    )
+    # Customize the tooltip template
+    fig_league.update_traces(
+        hovertemplate="<b>%{hovertext}</b><br><br>" +
+                  "Gameweek: %{x}<br>" +
+                  "Points: %{y}<br>" +
+                  "<extra></extra>"
     )
     fig_league.write_html("docs/Graphs/cumulative_points_chart.html")
 
